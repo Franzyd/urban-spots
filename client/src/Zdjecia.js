de5efa81b53341cd5784664e1a40bf07b43bb40d
Franzyd <firekfrank@gmail.com>
2024-01-13 00:48:13 +0100

    Add database

---
+++ client/src/Zdjecia.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import './Zdjecia.css'
+
+export default function Home() {
+
+    // const [backendData, setBackendData] = useState([{}])
+
+    // useEffect(() => {
+    //     fetch("/api").then(
+    //         response => response.json()
+    //     ).then(
+    //         data => {
+    //             setBackendData(data)
+    //         }
+    //     )
+    // }, [])
+
+    // return (
+    //     <div>
+    //         {(typeof backendData.users === 'undefined') ? (
+    //             <p>Loading...</p>
+    //         ) : (
+    //             backendData.users.map((user, i) => (
+    //                 <p key={i}>{user}</p>
+    //             ))
+    //         )}
+    //     </div>
+    // )
+    
+    return (<Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Zdjecia />} />
+        </Route>
+    </Routes>)
+}
+
+function Layout() {
+    return (<>
+        <header>
+            <ul>
+                <li>
+                    <Link to="/">Zdjęcia</Link>
+                </li>
+            </ul>
+        </header>
+        <Outlet />
+    </>)
+}
+
+function Zdjecia() {
+    const [spots, setSpots] = useState([{}])
+
+    useEffect(() => {
+        fetch("/spots").then(
+            response => response.json()
+        ).then(
+            data => {
+                setSpots(data)
+            }
+        )
+    }, [])
+
+    return (<main>
+        {(typeof spots.imagename === 'undefined') ? (
+            <p>Loading...</p>
+        ) : (
+            spots.map((spot, i) => (
+                <article key={i}>
+                    <img src={process.env.PUBLIC_URL + '/spot-images/' + spot.imagename} />
+                </article>
+            ))
+        )}
+    </main>)
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const app = express()
+const db = require("./database.js")
 
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
+})
+
+app.get("/api/users", (req, res, next) => {
+    const sql = "select * from user"
+    const params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message})
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    })
+})
+
+app.get("/spots", (req, res, next) => {
+    const sql = "select * from spot limit 30"
+    const params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) res.status(400).json({"error":err.message})
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    })
 })
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
